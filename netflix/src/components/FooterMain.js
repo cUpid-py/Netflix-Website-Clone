581a3fa7d29c45ec88383421961bc8e05bc47a6d
Pap-Ski <niilaryeaquarteypapafio@gmail.com>
2021-06-18 00:12:55 +0000

    responsiveness added for 500px max-width

--- netflix/src/components/FooterMain.js
+++ netflix/src/components/FooterMain.js
@@ -1,29 +1,40 @@
-import React from 'react'
-import "./FooterMain.css"
+import React from "react";
+import "./FooterMain.css";
 
 const FooterMain = () => {
-    return (
-        <div>
-            <div className="footer-1" > 
-                <a href=""> Questions? Contact Us</a>
-            </div>
-            
-            <div className="footer" >
-                
-                {/* <div className="footer-sub" > */}
-                    <div className="f1" > FAQ <br /> Investor Relations <br /> Privacy <br /> Speed Test </div>
-                    <div className="f2" > Help Center <br /> Jobs <br /> Cookie Preferences <br /> Legal Notices </div>
-                    <div className="f3" > Account <br /> Ways to Watch <br /> Corporate Information <br /> Netflix Originals </div>
-                    <div className="f4" > Media Center <br /> Terms of Use <br /> Contact Us </div>
-                {/* </div> */}              
-            </div>
+  return (
+    <div>
+      <div className="footer-1">
+        <a href=""> Questions? Contact Us</a>
+      </div>
 
-            <div className="footer-2" > 
-                <a href="#">Netflix Ghana</a> 
-            </div>
-            
+      <div className="footer">
+        {/* <div className="footer-sub" > */}
+        <div className="f1">
+          {" "}
+          FAQ <br /> Investor Relations <br /> Privacy <br /> Speed Test{" "}
         </div>
-    )
-}
+        <div className="f2">
+          {" "}
+          Help Center <br /> Jobs <br /> Cookie Preferences <br /> Legal Notices{" "}
+        </div>
+        <div className="f3">
+          {" "}
+          Account <br /> Ways to Watch <br /> Corporate Information <br />{" "}
+          Netflix Originals{" "}
+        </div>
+        <div className="f4">
+          {" "}
+          Media Center <br /> Terms of Use <br /> Contact Us{" "}
+        </div>
+        {/* </div> */}
+      </div>
 
-export default FooterMain
+      <div className="footer-2">
+        <a href="#">Netflix Ghana</a>
+      </div>
+    </div>
+  );
+};
+
+export default FooterMain;
--- netflix/src/components/LandingPage.js
+++ netflix/src/components/LandingPage.js
@@ -1,38 +1,40 @@
-import React from 'react'
-import "./LandingPage.css"
-import Logo from "../Assets/Logo.png"
+import React from "react";
+import "./LandingPage.css";
+import Logo from "../Assets/Logo.png";
 
 const landingPage = () => {
-    return (
-        <div>
-            <section className="bg">
-                <div className="nav-dec">
-                    <img className="Logo" src={Logo} alt="Logo"></img>
+  return (
+    <div>
+      <section className="bg">
+        <div className="nav-dec">
+          <img className="Logo" src={Logo} alt="Logo"></img>
 
-                    <a href="#"><button className="btn-1">Sign In</button></a>
-                </div>
-
-                <div className="Unlimited" >
-                    Unlimited movies, TV <br /> shows, and more.
-                </div>
-
-                <p className="watch" >Watch anywhere. Cancel anytime.</p>
-                <p className ="ready">Ready to watch? Enter your email to create or restart your membership.</p>
-
-                <div className="mail-but" >
-                    <input className="email-box" type="email" placeholder="Email Address"></input>
-                    <button className="gs-button" >Get Started</button>
-
-                </div>
-                
-
-
-            </section>
-           
-            
-            
+          <a href="#">
+            <button className="btn-1">Sign In</button>
+          </a>
         </div>
-    )
-}
 
-export default landingPage
+        <div className="getStarted">
+          <div className="Unlimited">
+            Unlimited movies, TV <br /> shows, and more.
+          </div>
+          <p className="watch">Watch anywhere. Cancel anytime.</p>
+          <p className="ready">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+          <div className="mail-but">
+            <input
+              className="email-box"
+              type="email"
+              placeholder="Email Address"
+            ></input>
+            <button className="gs-button">Get Started</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default landingPage;
